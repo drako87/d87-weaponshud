ff64024bb3234b6bb7beae2012f2bd0a21d7ca3c
Drako87 <drakogamer87@gmail.com>
2026-06-26 15:56:40 +0200

    Format fxmanifest.lua for consistency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Drako87/Dracatt'
-description 'D87 Weapons HUD - Interfaz táctica de armamento minimalista premium para Qbox'
+description 'D87 Weapons HUD - Interfaz táctica de armamento minimalista premium'
 version '1.0.0'
 
 -- Inicialización de ox_lib para soporte nativo
