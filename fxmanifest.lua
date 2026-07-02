fe7c7c60c3a7acfc20f2967c5111a85a46617238
Drako87 <drakogamer87@gmail.com>
2026-07-02 10:49:26 +0200

    Update fxmanifest.lua for version 1.1.0
    Updated version to 1.1.0 and modified client/server scripts.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,15 @@ game 'gta5'
 
 author 'Drako87/Dracatt'
 description 'D87 Weapons HUD - Interfaz táctica de armamento minimalista premium'
-version '1.0.0'
+version '1.1.0'
 
--- Inicialización de ox_lib para soporte nativo
+-- Inicialización de ox_lib para soporte nativo (usado también por el
+-- callback framework-agnóstico de munición de reserva)
 shared_script '@ox_lib/init.lua'
 
 shared_script 'config.lua'
-client_script 'client.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
 
 ui_page 'html/ui.html'
 
